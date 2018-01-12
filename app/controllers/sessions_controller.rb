3e065aa4c86b594686e374d5786bfde178aa2ee6
Kyle Lemon <james.kyle.lemon@gmail.com>
2018-01-12 11:05:21 -0500

    Implement remember login features

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
       flash[:success] = "You have logged in!"
-      log_in(user)
-      redirect_to user
+      log_in(@user)
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       flash.now[:danger] = 'Email or password incorrect. Try again.'
       render 'new'
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     flash[:success] = "You have logged out"
     redirect_to root_path
   end
